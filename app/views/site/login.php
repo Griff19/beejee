b97820432e10dc208ae8e8cece0244af6356e65f
griff19 <griff19@mail.ru>
2021-03-21 05:38:32 +0700

    форма авторизации

--- app/views/site/login.php
+++ app/views/site/login.php
@@ -9,13 +9,18 @@ use griff\App;
 <h2> <?= T::t('ENTER_LOGIN_AND_PASSWORD')?> </h2>
 
 <form action="<?= App::$root ?>site/login" method="post">
-    <label> <?= T::t('USR_LOGIN')?>: </label><br/>
-    <input name="login" type="text" size="15" maxlength="15"><br/>
-    
-    <label> <?= T::t('USR_PASS')?>: </label><br/>
-    <input name="password" type="password" size="15" maxlength="15"><br/><br/>
-    
-    <input type="submit" value="<?= T::t('LINK_SIGN_IN')?>"><br/><br/>
+    <div class="row">
+        <label class="col-sm-2 col-form-label"> <?= T::t('USR_LOGIN')?>: </label><br/>
+        <div class="col-sm-10">
+            <input class="form-control" name="login" type="text" size="15" maxlength="15"><br/>
+        </div>
+        <label class="col-sm-2 col-form-label"> <?= T::t('USR_PASS')?>: </label><br/>
+        <div class="col-sm-10">
+            <input class="form-control" name="password" type="password" size="15" maxlength="15"><br/><br/>
+        </div>
+        
+        <input class="btn btn-primary mt-3 mb-3" type="submit" value="<?= T::t('LINK_SIGN_IN')?>"><br/><br/>
+    </div>
 </form>
 
 <p>
